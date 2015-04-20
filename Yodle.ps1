efcbb7020faaab53dcf11a1c558f8fc529681f77
Devtr0n <richardsmailbox@gmail.com>
2015-04-20 11:05:57 -0500

    initial upload of code files

---
+++ Yodle.ps1
@@ -0,0 +1,4 @@
+﻿$total = 0
+$list=@(Invoke-webrequest -URI "http://www.yodlecareers.com/puzzles/triangle.txt").Content -split '\s+'					#PowerShell V3
+#$list=@(new-object Net.WebClient).DownloadString("http://www.yodlecareers.com/puzzles/triangle.txt") -split '\s+' | iex 	#PowerShell V2
+foreach ($item In $list) { $total += $item } "Total: " + '{0:n2}' -f $total 
